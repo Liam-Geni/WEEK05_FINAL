d8b3bb6881f32122c015e97cb7a70a7456df2df2
Liam-Genius <gijae8783@naver.com>
2022-10-20 18:43:47 +0900

    intial Commit

---
+++ src/main/java/com/sparta/week05team/Week05teamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week05team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week05teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week05teamApplication.class, args);
+	}
+
+}
